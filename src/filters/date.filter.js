2e976b3a27511646e3130a3d1b83c4c3c3d8783b
Vlad <vladislav.efanov@gmail.com>
2021-06-26 20:41:26 +0200

    Доработал меню и создал фильтр для даты.

---
+++ src/filters/date.filter.js
@@ -0,0 +1,19 @@
+export default function dateFilter (value, format = 'date') {
+  const options = {}
+
+  if (format.includes('date')) {
+    options.day = '2-digit'
+    options.month = 'long'
+    options.year = 'numeric'
+  }
+
+  if (format.includes('time')) {
+    options.hour = '2-digit'
+    options.minute = '2-digit'
+    options.second = '2-digit'
+  }
+
+  const date = new Date(value)
+
+  return new Intl.DateTimeFormat('fu-RU', options).format(date)
+}
